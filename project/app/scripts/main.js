25cf68484ea940131dbb74fff1f6a588d33ce57e
legraeff <leticia.graeff@gmail.com>
2016-07-11 00:22:19 -0300

    Removed fixed short links from HTML; Added function to load the URLs JSON dynamically from Github; Added function to build the top 5 list; Added function to make a short URL; Added treatment to short or copy the shortened URL; Added arrow-down in hits to html.

--- project/app/scripts/main.js
+++ project/app/scripts/main.js
@@ -1,11 +1,72 @@
+function isShort() {
+  return $('#short-and-copy').html() === 'ENCURTAR';
+}
 
-var buildTop5 = function (data) {
-  console.log(data);
+function inputKeyEvent(event){
+  if(event.keyCode == 13 && isShort()){
+    shortifyEvent();
+  }
+}
+
+function shortify(link){
+  var randomString = Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 5);
+  return 'http://chr.dc/' + randomString;
+}
+
+function clearTextEvent(){
+  $('#input-link').val('');
+  $('#short-and-copy').html('ENCURTAR');
+}
+
+function shortifyEvent(){
+  var link = $('#input-link').val();
+  var reg = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+
+  if(link.search(reg) == -1) {
+    alert("Link inválido!");
+    return;
+  }
+
+  var shortLink = shortify(link);
+  $('#input-link').val(shortLink);
+  $('#short-and-copy').html('COPIAR');
+}
+
+function copyEvent(){
+  $('#input-link').select();
+  document.execCommand('copy');
+}
+
+function shortAndCopyEvent(){
+  if(isShort()) {
+    shortifyEvent();
+  } else {
+    copyEvent();
+  }
+}
+
+function buildTop5(data){
+  var urlsData = JSON.parse(data);
+  urlsData.sort(function(a, b){return b.hits - a.hits;});
+
+  for (var i=0; i < 5; i++) {
+    var urlInfo = urlsData[i];
+    $('#urls').append('<li><a href="' +
+      urlInfo.url + '">' +
+      urlInfo.shortUrl + '</a><span>' +
+      urlInfo.hits + '</span></li>');
+  }
+
+  var sum = 0;
+  urlsData.forEach(function(item){
+    sum += item.hits;
+  });
+  $('#total-hits').append(sum);
 };
 
 function getURLs(){
   $.ajax({
-    url: "../Assets/urls.json",
+    url: 'https://raw.githubusercontent.com/legraeff/frontend-intern-challenge/leticia-graeff/Assets/urls.json',
     cache: false,
     success: buildTop5
   });
