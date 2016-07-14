7980cf7982cf31d26fa78190f3a383895c9752be
legraeff <leticia.graeff@gmail.com>
2016-07-14 19:42:44 -0300

    Fixed final issues after feedback (background image and fixed most global scope functions).

--- project/app/scripts/main.js
+++ project/app/scripts/main.js
@@ -1,45 +1,37 @@
-
 //Verify if the shortify button is enabled.
 function isShort() {
   return $('#short-and-copy').html() === 'ENCURTAR';
 }
 
-//Generates random, short URL
-function shortify(link){
-  var randomString = Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 5);
-  return 'http://chr.dc/' + randomString;
-}
-
 //Validates the input URL, outputs the short URL into the input field.
 function shortifyEvent(){
-  var link = $('#input-link').val();
+  var input = $('#input-link');
+  var clear = $('#clear');
+  var shortAndCopy = $('#short-and-copy');
+  var notification = $('#notification');
+
+  var link = input.val();
   var reg = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
   if(link.search(reg) == -1) {
-    $('#notification').html('Link inválido!');
+    notification.html('Link inválido!');
     return;
   }
-  var input = $("#input-link");
-  var clear = $("#clear");
-  var shortAndCopy = $("#short-and-copy");
-  $('#notification').html('');
-  var shortLink = shortify(link);
-  $(input).val(shortLink);
-  $(input).addClass('blink');
-  $(clear).removeClass('unclickable');
-  $(clear).addClass('blink');
-  $(shortAndCopy).text('COPIAR');
-  $(shortAndCopy).addClass('blink');
-}
+  notification.html('');
 
-//Copies the output, short URL.
-function copyEvent(){
-  $('#input-link').select();
-  document.execCommand('copy');
-  $('#notification').html('Copiado com sucesso!');
+  //Generates random, short URL
+  var randomString = Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 5);
+  var shortLink = 'http://chr.dc/' + randomString;
+
+  input.val(shortLink);
+  input.addClass('blink');
+  clear.removeClass('unclickable');
+  clear.addClass('blink');
+  shortAndCopy.text('COPIAR');
+  shortAndCopy.addClass('blink');
 }
 
 //Fire function with Enter key
-$("#input-link").keyup(function inputKeyEvent(event){
+$('#input-link').keyup(function inputKeyEvent(event){
   if(event.keyCode == 13 && isShort()){
     shortifyEvent();
   }
@@ -50,51 +42,49 @@ $('#short-and-copy').click(function shortAndCopyEvent(){
   if(isShort()) {
     shortifyEvent();
   } else {
-    copyEvent();
+    //Copies the output, short URL.
+    $('#input-link').select();
+    document.execCommand('copy');
+    $('#notification').html('Copiado com sucesso!');
   }
 });
 
 //Clears the input field, resets color, notification, shortify and clear button.
 $('#clear').click(function clearTextEvent() {
-  var input = $("#input-link");
-  var clear = $("#clear");
-  var shortAndCopy = $("#short-and-copy");
-  $(input).val('');
-  $(input).removeClass('blink');
-  $('#notification').html('');
-  $(clear).addClass('unclickable');
-  $(clear).removeClass('blink');
-  $(shortAndCopy).removeClass('blink');
-  $(shortAndCopy).html('ENCURTAR');
+  var input = $('#input-link');
+  var clear = $('#clear');
+  var shortAndCopy = $('#short-and-copy');
+  var notification = $('#notification');
+  input.val('');
+  input.removeClass('blink');
+  notification.html('');
+  clear.addClass('unclickable');
+  clear.removeClass('blink');
+  shortAndCopy.removeClass('blink');
+  shortAndCopy.html('ENCURTAR');
 });
 
-//Sorts the URLS in the JSON by hits. Appends the Top 5 and the sum of total hits into the HTML.
-function buildTop5(data){
-  var urlsData = JSON.parse(data);
-  urlsData.sort(function(a, b){return b.hits - a.hits;});
-
-  for (var i=0; i < 5; i++) {
-    var urlInfo = urlsData[i];
-    $('#urls').append('<li><a href="' +
-      urlInfo.url + '">' +
-      urlInfo.shortUrl + '</a><span>' +
-      urlInfo.hits + '</span></li>');
-  }
-
-  var sum = 0;
-  urlsData.forEach(function(item){
-    sum += item.hits;
-  });
-  $('#total-hits').append(sum);
-};
-
 //Gets URLS from the JSON.
-function getURLs(){
+(function getURLs(){
   $.ajax({
     url: 'https://raw.githubusercontent.com/legraeff/frontend-intern-challenge/leticia-graeff/Assets/urls.json',
     cache: false,
-    success: buildTop5
+//Sorts the URLS in the JSON by hits. Appends the Top 5 and the sum of total hits into the HTML.
+    success: function (data){
+      var urlsData = JSON.parse(data);
+      urlsData.sort(function(a, b){return b.hits - a.hits;});
+      for (var i=0; i < 5; i++) {
+        var urlInfo = urlsData[i];
+        $('#urls').append('<li><a href="' +
+            urlInfo.url + '">' +
+            urlInfo.shortUrl + '</a><span>' +
+            urlInfo.hits + '</span></li>');
+      }
+      var sum = 0;
+      urlsData.forEach(function(item){
+        sum += item.hits;
+      });
+      $('#total-hits').append(sum);
+    }
   });
-}
-
-getURLs();
+})();
